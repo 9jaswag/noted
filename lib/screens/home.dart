e860ac2ffeccfc495b4959ac968d5e2ec6bb5c1e
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-03-31 22:48:51 +0100

    - add search field - add user avatar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:noted/common/app_drawer.dart';
 
 class Home extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +20,26 @@ class Home extends StatelessWidget {
                   child: Material(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(8.0),
-                    elevation: 5.0,
+                    elevation: 2.0,
                     child: ListTile(
                       leading: IconButton(
                         icon: Icon(
                           Icons.menu,
                         ),
                         onPressed: () => _scaffoldKey.currentState.openDrawer(),
+                      ),
+                      title: TextField(
+                        controller: controller,
+                        decoration: InputDecoration.collapsed(
+                          hintText: "Search your notes",
+                        ),
+                        autofocus: false,
+                        onChanged: null,
+                        onTap: null,
+                      ),
+                      trailing: CircleAvatar(
+                        backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1032266310586122240/0jAWyzF3_400x400.jpg'),
+                        radius: 15.0,
                       ),
                     ),
                   ),
@@ -38,4 +52,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-// Scaffold.of(context).openDrawer();
