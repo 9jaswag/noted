bed926298b4f47c956dfae3dd4a8a7702f990286
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-03-31 22:27:46 +0100

    - create custom app bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,11 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:noted/common/app_drawer.dart';
 
 class Home extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Title')),
+      key: _scaffoldKey,
       drawer: AppDrawer(),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Material(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    elevation: 5.0,
+                    child: ListTile(
+                      leading: IconButton(
+                        icon: Icon(
+                          Icons.menu,
+                        ),
+                        onPressed: () => _scaffoldKey.currentState.openDrawer(),
+                      ),
+                    ),
+                  ),
+                )
+              ]),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+// Scaffold.of(context).openDrawer();
